0897ec4bb50efc536da87fe5530765d859b48469
bennyperez-cpu <55594332+bennyperez-cpu@users.noreply.github.com>
2023-04-01 20:56:53 +0200

    Update 1Abr2023

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -33,11 +33,53 @@ async def users():
 
 @app.get("/user/{id}")
 async def user(id: int):
+    return search_user(id)
+
+def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
         return "{Error: No se ha encontrado el usuario}"
+    
+@app.post("/user/")
+async def user(user: User): 
+    if type (search_user(user.id)) == User:
+       return "{Error: El usuario ya existe}"
+    else:
+        users_list.append(user)
+        return user
+
+
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"Error": "No se ha actualizado el usuario"}
+    else:
+        return user
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"Error": "No se ha eliminado el usuario"}
+    else:
+        return {"El usuario se ha eliminado satisfactoriamente"}   
+
+
+
+
 
 #Url local: http://127.0.0.1:8000/url
 
