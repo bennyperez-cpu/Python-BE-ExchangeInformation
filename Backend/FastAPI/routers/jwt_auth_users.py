86b1a86ff5dc68cead89c54c385897b4ebe5b4e5
bennyperez-cpu <55594332+bennyperez-cpu@users.noreply.github.com>
2023-04-06 11:57:52 +0200

    6Abr2023_1

--- Backend/FastAPI/routers/jwt_auth_users.py
+++ Backend/FastAPI/routers/jwt_auth_users.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt
+from passlib.context import CryptContext
+from datetime
 
 app = FastAPI()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+crypt = CryptContext(schemes = ["bcrypt"])
 
 
 class User(BaseModel):
@@ -23,42 +28,24 @@ users_db = {
     "full_name": "Benny Hammer",
     "email": "benny.perez@pucp.pe",
     "disabled": False,
-    "password": "123456"
+    "password": "$2a$12$cxC/LFlm8EZHDH7KZJteg./zVslc/75x3ZxZ/NjsKp2oSq77JDj5O"
     },
     "quantum2": {
     "username": "Quantum2",
     "full_name": "Benny Hammer",
     "email": "benny.perez@pucp.pe",
     "disabled": True,
-    "password": "654321"
+    "password": "$2a$12$qy4eAvY5bK2o0GoiNaGmlOblELHLSUeCkjiHOCaFmnHtcvfmgL5mS"
     }
         
 }
-    
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = 1
 
 def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
-    
-def search_user(username: str):
-    if username in users_db:
-        return User(**users_db[username])   
-    
-async def current_user(token: str = Depends(oauth2)):
-    user = search_user(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
-            detail="Credenciales de autenticación inválidas",
-            headers ={"WWW-Authenticate": "Bearer"})
-    if user.disabled:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST , 
-            detail="Usuario inactivo")
-
-
-    return user
-
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
@@ -67,12 +54,12 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
         raise HTTPException(
             status_code=400, detail="El usuario no es correcto")
     user = search_user_db(form.username)
-    if not form.password == user.password:
+
+    
+
+
+    if not crypt.verify(form.password, user.password):
         raise HTTPException(
             status_code=400, detail="La contraseña no es correcta")
     
     return {"access_token": user.username, "token_type": "bearer"}
-
-@app.get("/users/me")
-async def me(user: User = Depends(current_user)):
-    return user
