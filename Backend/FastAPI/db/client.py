3ce5dbcf6748794969214a2e6fd84a8ae4903998
bennyperez-cpu <55594332+bennyperez-cpu@users.noreply.github.com>
2023-04-30 13:25:46 +0200

    30Abr2023_1

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
+#Base de Datos Local
+# db_client = MongoClient("mongodb://192.168.1.51:27017/").local
 
-db_client = MongoClient("mongodb://192.168.1.51:27017/").local
-
+#Base de Datos Remota
+db_client = MongoClient("mongodb+srv://mrbennyhammer:mrbennyhammer@cluster0.qwlud3w.mongodb.net/?retryWrites=true&w=majority").test
 
